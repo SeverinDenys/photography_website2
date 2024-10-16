2dce90ea574cdbc1e224e9704a00e0fa526bd620
Denys Severin <severindenys367@gmail.com>
2024-10-16 14:31:30 +0200

    design signIn component/ adding states

--- src/components/form/FormSign.jsx
+++ src/components/form/FormSign.jsx
@@ -1,20 +1,71 @@
 import { Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
-export default function MainPhotoSessionForm() {
-  const navigate = useNavigate();
+import {
+  TextField,
+  Button,
+  Typography,
+  Container,
+  Box,
+} from "@mui/material";
+import React, { useState } from "react";
 
+export default function MainPhotoSessionForm() {
   const onLogin = () => {
     console.log("hey");
-    // navigate("http://denys.localhost:3000");
     window.location.href = "http://denys.localhost:3000";
   };
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <>
-      <div>SIGN IN</div>;
-      <input type="text" />
-      <input type="text" />
-      <button onClick={onLogin}>LOGIN</button>
-      <Link to="/signUp">SIGN UP</Link>
-    </>
+    <Container maxWidth="xs">
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          minHeight: "100vh",
+        }}
+      >
+        <Typography variant="h4">SIGN IN</Typography>
+
+        <TextField
+          label="Email"
+          variant="outlined"
+          margin="normal"
+          fullWidth
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <TextField
+          label="Password"
+          type="password"
+          variant="outlined"
+          margin="normal"
+          fullWidth
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <Button
+          variant="contained"
+          color="primary"
+          fullWidth
+          onClick={onLogin}
+          sx={{ mt: 2 }}
+        >
+          LOGIN
+        </Button>
+
+        <Typography variant="body2" sx={{ mt: 2 }}>
+          Don't have an account?{" "}
+          <Link to="/signUp" style={{ textDecoration: "none" }}>
+            Sign Up
+          </Link>
+        </Typography>
+      </Box>
+    </Container>
   );
 }
